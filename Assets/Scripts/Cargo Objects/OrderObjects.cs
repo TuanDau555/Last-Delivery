71c18189b925c91c2c8a70da32a9a8c35b5c8cf5
TuanDau555 <nguyenmtuan505@gmail.com>
2025-10-06 21:15:14 +0700

    Checking if player already has obj

--- Assets/Scripts/Cargo Objects/OrderObjects.cs
+++ Assets/Scripts/Cargo Objects/OrderObjects.cs
@@ -6,16 +6,26 @@ public class OrderObjects : BaseInteract
     {
         base.Interact(playerController);
 
-        // Get Order
-        CargoObjectSO order = DeliveryManager.Instance.AddOrder();
 
         // Give the prefabs instruction for player
-        Transform cargoOrder = Instantiate(order.cargoOrderPrefab);
-        CargoObject cargo = cargoOrder.GetComponent<CargoObject>();
-        cargo.SetCargoObjectSO(order);
-        cargo.SetObjectParent(playerController);
+        if (!playerController.HasCargoObject())
+        {
+            // Get Order
+            CargoObjectSO order = DeliveryManager.Instance.AddOrder();
+        
+            Transform cargoOrder = Instantiate(order.cargoOrderPrefab);
+            CargoObject cargo = cargoOrder.GetComponent<CargoObject>();
+        
+            cargo.SetObjectParent(playerController);
+            cargo.SetCargoObjectSO(order);
+        
+            DeliveryManager.Instance.SetCurrentDeliveryObject(order);
+            Debug.Log("You have an order to delivery");
+        }
+        else
+        {
+            Debug.LogWarning("You already hold something");
+        }
 
-        DeliveryManager.Instance.SetCurrentDeliveryObject(order);  
-        Debug.Log("You have an order to delivery");
     }
 }
