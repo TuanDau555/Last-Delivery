3f12dfaf6fa3a601814cf0e9b754e61e9564d0c3
TuanDau555 <nguyenmtuan505@gmail.com>
2025-10-14 21:03:57 +0700

    Fixing cat stop while deliver
    Only stop the cat if there no object to deliver, and clear order object only not clear cargo object when delivery

--- Assets/Scripts/Cargo Objects/TrashBin.cs
+++ Assets/Scripts/Cargo Objects/TrashBin.cs
@@ -12,11 +12,23 @@ public class TrashBin : BaseInteract
             CargoObjectSO cargoSO = cargoObject.GetCargoObjectSO();
 
             DeliveryManager.Instance.RemoveOrder(cargoSO);
-            DeliveryManager.Instance.ClearDeliveryObject();
 
-            
+            // Just want to call this method if it delivery object
+            if(DeliveryManager.Instance.currentDeliveryObject == cargoSO)
+            {
+                Debug.Log($"You have throw {DeliveryManager.Instance.currentDeliveryObject}");
+                DeliveryManager.Instance.ClearDeliveryObject();
+            }
+
             cargoObject.DestroySelf();
             Debug.Log($"You have threw into trash bin");
+
+            // Cat only stop when there NO order to delivery
+            if (!DeliveryManager.Instance.HasPendingOrder())
+            {
+                DeliveryManager.Instance.TriggerStopDelivery();
+                Debug.Log("There are no object in List cat stop");
+            }
         }
 
         else
