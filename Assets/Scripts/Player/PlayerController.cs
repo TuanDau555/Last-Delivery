a3ae1eeed16d68807ab4eae5fd253b6f7fa85dcb
TuanDau555 <nguyenmtuan505@gmail.com>
2025-10-14 21:13:02 +0700

    Add nav surface and update player Stats
    Nav mesh  for cat and Enemy

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour, IObjectParent
