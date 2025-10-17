0a366f8381437ac3c93fa3eb577f2cc707301e36
TuanDau555 <nguyenmtuan505@gmail.com>
2025-10-17 11:33:03 +0700

    Add: implement FileDataHandler for saving and loading data

---
+++ Assets/Scripts/Save and Load/FileDataHandler.cs
@@ -0,0 +1,91 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+public class FileDataHandler
+{
+    private string saveDirPath = "";
+    private string saveFileName = "";
+    private string fullSavePath = "";
+
+    #region Constructor
+    public FileDataHandler(string saveDirPath, string saveFileName)
+    {
+        this.saveDirPath = saveDirPath;
+        this.saveFileName = saveFileName;
+    }
+    #endregion
+
+    #region Path
+    // Set the save path on difference OS
+    public string SavePath()
+    {
+        fullSavePath = Path.Combine(saveDirPath, saveFileName);
+        return fullSavePath;
+    }
+    #endregion
+
+    #region Save/Load
+    public SaveData LoadFile()
+    {
+        SaveData LoadData = null;
+
+        if (File.Exists(SavePath()))
+        {
+            try
+            {
+                string dataToLoad = "";
+
+                // Load serialized data from file
+                using (FileStream stream = new FileStream(SavePath(), FileMode.Open))
+                {
+                    // Open stream
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        // read stream
+                        dataToLoad = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Failed to load file: {e.Message}");
+            }
+        }
+        else
+        {
+            Debug.LogError($"Save file does not exist at {SavePath()}");
+        }
+
+        return LoadData;
+    }
+
+    public void SaveFile(SaveData data)
+    {
+        try
+        {
+            // Create the direct if it doesn't exits
+            if (!Directory.Exists(saveDirPath))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(SavePath()));
+            }
+
+            // Serialized data to Json
+            string json = JsonUtility.ToJson(data, true);
+
+            // Write data to file
+            using(FileStream stream = new FileStream(SavePath(), FileMode.Create))
+            {
+                using(StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(json);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to save file: {e.Message}");
+        }
+    }
+    #endregion
+}
