b099d53b007b738e3a5aca6ff28fc19a0137d7c2
TuanDau555 <nguyenmtuan505@gmail.com>
2025-10-17 10:26:50 +0700

    Separate region

--- Assets/Scripts/AI/Enemy/ChaseState.cs
+++ Assets/Scripts/AI/Enemy/ChaseState.cs
@@ -11,18 +11,15 @@ public class ChaseState : EnemyBaseState
     private FieldOfView _fov;
     private float _viewRadius;
     private float _viewAngle;
-
-    private bool _useMovePrediction; // just for test when comparison without prediction 
     #endregion
 
     #region Constructor 
-    public ChaseState(Enemy enemy, NavMeshAgent agent, PlayerController player, FieldOfView fov, EnemyStatsSO statsSO, bool usePrediction) : base(enemy)
+    public ChaseState(Enemy enemy, NavMeshAgent agent, PlayerController player, FieldOfView fov, EnemyStatsSO statsSO) : base(enemy)
     {
         this._navMeshAgent = agent;
         this._player = player;
         this._fov = fov;
         this._statsSO = statsSO;
-        this._useMovePrediction = usePrediction;
     }
     #endregion
 
@@ -63,51 +60,44 @@ public class ChaseState : EnemyBaseState
     #region States
     private void ChasePlayer()
     {
-        // repetitively updated player position
-        if (!_useMovePrediction)
+        // Estimate player's move and time, which enemy needs to go to player position  
+        float timeToPlayer = Vector3.Distance(_player.transform.position, _navMeshAgent.transform.position)
+        / _statsSO.stats.chaseSpeed;
+
+        // To avoid Prediction player move to far
+        // ìf player goes further, the enemy can only prediction the amount of time (here is movePredictionTime) 
+        if (timeToPlayer > _statsSO.stats.movePredictionTime)
         {
-            _navMeshAgent.SetDestination(_player.transform.position);
-        }
-        else
-        {
-            // Estimate player's move and time, which enemy needs to go to player position  
-            float timeToPlayer = Vector3.Distance(_player.transform.position, _navMeshAgent.transform.position)
-            / _statsSO.stats.chaseSpeed;
-
-            // To avoid Prediction player move to far
-            // ìf player goes further, the enemy can only prediction the amount of time (here is movePredictionTime) 
-            if (timeToPlayer > _statsSO.stats.movePredictionTime)
-            {
-                timeToPlayer = _statsSO.stats.movePredictionTime;
-            }
-
-            // Prediction the position that player may come
-            // This current position plus with Distance that player may go in that time
-            /// Summary:
-            ///     If the player runs in direction X with an average speed of 5m/s and it takes me 1s to catch up to the same place → then the player has gone another 5m, I should run there.
-            Vector3 targetPosition = _player.transform.position
-                        + _player.GetAverageVelocity() * timeToPlayer;
-
-
-            Vector3 directionToPlayer = (_player.transform.position - _navMeshAgent.transform.position).normalized;
-            Vector3 directionToTarget = (targetPosition - _navMeshAgent.transform.position).normalized;
-
-            float dot = Vector3.Dot(directionToPlayer, directionToTarget); // cosin angle between 2 vector
-
-            // if enemy's prediction direction is too far from current player's direction...
-            // To Prevent run infront of player
-            if (dot < _statsSO.stats.movePredictionThreshold)
-            {
-                // ...No need to predict go to player position 
-                targetPosition = _player.transform.position;
-            }
-
-            _navMeshAgent.SetDestination(targetPosition);
+            timeToPlayer = _statsSO.stats.movePredictionTime;
         }
 
+        // Prediction the position that player may come
+        // This current position plus with Distance that player may go in that time
+        /// Summary:
+        ///     If the player runs in direction X with an average speed of 5m/s and it takes me 1s to catch up to the same place → then the player has gone another 5m, I should run there.
+        Vector3 targetPosition = _player.transform.position
+                    + _player.GetAverageVelocity() * timeToPlayer;
+
+
+        Vector3 directionToPlayer = (_player.transform.position - _navMeshAgent.transform.position).normalized;
+        Vector3 directionToTarget = (targetPosition - _navMeshAgent.transform.position).normalized;
+
+        float dot = Vector3.Dot(directionToPlayer, directionToTarget); // cosin angle between 2 vector
+
+        // if enemy's prediction direction is too far from current player's direction...
+        // To Prevent run infront of player
+        if (dot < _statsSO.stats.movePredictionThreshold)
+        {
+            // ...No need to predict go to player position 
+            targetPosition = _player.transform.position;
+        }
+
+        _navMeshAgent.SetDestination(targetPosition);
 
     }
-
+    #endregion
+        
+    #region Change State
     private void InAttackRange()
     {
         float distance = Vector3.Distance(_navMeshAgent.transform.position, _player.transform.position);
