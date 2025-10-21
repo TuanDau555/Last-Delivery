a411d587b0e539c975aea37f65cf167be6f40a68
TuanDau555 <nguyenmtuan505@gmail.com>
2025-10-21 21:57:49 +0700

    Interact with Shop Item

--- Assets/Scripts/SO/ShopItemSO.cs
+++ Assets/Scripts/SO/ShopItemSO.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Objects", menuName = "Objects/Shop Item")]
@@ -16,8 +17,20 @@ public class ShopItemSO : ScriptableObject
     [Space(10)]
     [Header("Gameplay")]
     public bool isCatItem;
+    public bool isUpgradeItem;
+    
+    [Tooltip("Max Stats Upgrade")]
+    [Range(1f, 100f)]
+    public float upgradeValue;
 
-    [Tooltip("Percentage of the buff applied")]
-    [Range(1f, 50f)]
-    public float buffValue;
+    [Range(0f, 50f)]
+    public float catMoodBuff;
+    [Range(0f, 50f)]
+    public float playerSpeedBuff;
+    [Range(0f, 50f)]
+    public float playerHPBuff;
+
+    [Tooltip("buff expiration time")]
+    [Range(0f, 360f)]
+    public float buffTime;
 }
