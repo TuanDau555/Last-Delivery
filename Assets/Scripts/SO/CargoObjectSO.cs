cb4dbb7e352ae8a80feaceb41c23d810e847c38c
TuanDau555 <nguyenmtuan505@gmail.com>
2025-11-09 21:30:34 +0700

    Update: object only use in Lv2

--- Assets/Scripts/SO/CargoObjectSO.cs
+++ Assets/Scripts/SO/CargoObjectSO.cs
@@ -5,6 +5,9 @@ public class CargoObjectSO : ScriptableObject
 {
     public string id;
     public string objectName;
+
+    [Tooltip("Lost object is only use in Lv2 Scene")]
+    public bool isLostItem;
     [Tooltip("The object when player take an order")]
     public Transform cargoOrderPrefab;
     [Tooltip("The object to delivery")]
