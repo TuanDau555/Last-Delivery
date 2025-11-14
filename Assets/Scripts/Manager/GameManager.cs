4f009741770a08ad3865e68ae83baab15a614873
TuanDau555 <nguyenmtuan505@gmail.com>
2025-11-14 20:54:38 +0700

    Add: Game Manger (Pause, New/Load Game, Resume,...)

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    [SerializeField] private GameObject gamePausePanel;
+    
+    private InputManager _inputManager;
+
+    #region Execute
+    void Start()
+    {
+        _inputManager = InputManager.Instance;
+    }
+
+    void Update()
+    {
+        PauseGame();
+    }
+    #endregion
+
+    #region Main Menu
+    public void CreateNewGame()
+    {
+        SaveManager.Instance.NewGame();
+    }
+
+    public void ContinueGame()
+    {
+        SaveManager.Instance.LoadGame();
+    }
+    #endregion
+    
+    #region Pause menu
+    public void RestartDay()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync("Lv1");
+        SaveManager.Instance.LoadCheckpoint();
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.None;
+        gamePausePanel.SetActive(false);
+    }
+    
+    public void PauseGame()
+    {
+        if (_inputManager.IsPause())
+        {
+            Time.timeScale = 0f;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            gamePausePanel.SetActive(true);    
+        }
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }    
+    #endregion
+
+    #region Both
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+}
