dfcf3cc79b277b094ae1a8382a1dd3186d4fdc64
TuanDau555 <nguyenmtuan505@gmail.com>
2025-11-22 19:03:18 +0700

    Add: Flashlight controller

---
+++ Assets/Scripts/Other/FlashLightController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class FlashLightController : MonoBehaviour
+{
+    [SerializeField] private Light flashLight;
+    private bool _isOn = false;
+
+    #region Execute
+    void Start()
+    {
+        if(flashLight == null)
+        {
+            flashLight = GetComponentInChildren<Light>();
+
+            flashLight.enabled = _isOn;
+        }
+    }
+
+    void Update()
+    {
+        if (InputManager.Instance.IsOpenFlashLight())
+        {
+            ToggleFlashLight();
+        }
+    }
+
+    private void ToggleFlashLight()
+    {
+        _isOn = !_isOn;
+        flashLight.enabled = _isOn;
+    }
+    #endregion
+}
