5b1d4bef3edf80a5c69b8476c453527207c0bde4
TuanDau555 <nguyenmtuan505@gmail.com>
2025-11-22 19:02:00 +0700

    Update: Game Over and delete object Persistent Singleton

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -5,18 +5,32 @@ public class GameManager : Singleton<GameManager>
 {
 
     [SerializeField] private GameObject gamePausePanel;
+    [SerializeField] private GameObject gameOverPanel;
     
     private InputManager _inputManager;
 
     #region Execute
-    void Start()
+    private void Start()
     {
         _inputManager = InputManager.Instance;
+        gameOverPanel.SetActive(false);
+        gamePausePanel.SetActive(false);
     }
 
-    void Update()
+    private void Update()
     {
         PauseGame();
+    }
+    #endregion
+    
+    #region End Game
+    public void GameOver()
+    {
+        Time.timeScale = 0f;
+        gameOverPanel.SetActive(true);
+        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
     #endregion
     
@@ -38,6 +52,7 @@ public class GameManager : Singleton<GameManager>
     
     public void PauseGame()
     {
+        if(SceneManager.GetActiveScene().name == "Menu") return;
         if (_inputManager.IsPause())
         {
             Time.timeScale = 0f;
@@ -49,7 +64,18 @@ public class GameManager : Singleton<GameManager>
 
     public void BackToMenu()
     {
+        // Refresh object's list to save correct object in current Scene
+        SaveManager.Instance.RefreshSaveables();
+        SaveManager.Instance.SaveGame();
+
+        ResetCoreSingletons();
         SceneManager.LoadScene("Menu");
+    }
+
+    private void ResetCoreSingletons()
+    {
+        Destroy(WorldManager.Instance?.gameObject);
+        Destroy(InputManager.Instance?.gameObject);
     }    
     #endregion
 }
